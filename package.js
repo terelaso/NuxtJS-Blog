b362af7a7cbbd099d02d15929ed2105791c84d65
terelaso <166241935+terelaso@users.noreply.github.com>
2025-05-07 20:57:13 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+    "name": "nuxt-blog",
+    "version": "1.0.0",
+    "scripts": {
+      "dev": "yans.soc",
+      "build": "nuxt build",
+      "generate": "nuxt generate",
+      "preview": "nuxt preview"
+    },
+    "dependencies": {
+      "nuxt": "^3.12.0"
+    }
+  }  
